e75a2c3be10eecf1896852734d76c96f2bba6c9b
PRO <PRO@DESKTOP-MSSB8T3>
2024-07-14 20:50:38 +0900

    초기 커밋.

---
+++ TableOrderClient/main.cpp
@@ -0,0 +1,173 @@
+#include <WinSock2.h>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <sstream>
+#include <thread>
+#include <Windows.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+#define PORT	8080
+#define IP		"127.0.0.1"
+
+int count = 0;
+SOCKET		hSocket;
+
+using namespace std;
+
+string loginProcess();
+string menuListRequest();
+void menuSelect();
+void showError(const char* msg);
+
+int state = 0;
+string account_id, account_passwd, store_nm, table_id;
+
+void proc_recv() {
+	char buffer[10000] = {};
+	string data;
+	while (!WSAGetLastError()) {
+		ZeroMemory(&buffer, 10000);
+		recv(hSocket, buffer, 10000, 0);
+		cout << "받은 메세지 : " << buffer << endl;
+		data = buffer;
+		data = data.substr(data.find_first_of('{'));
+
+		if (data.find("ACCOUNT_LOGIN")) {
+			stringstream datalist;
+			string tmp, msgCD, msg;
+			datalist.clear();
+			datalist << data;
+			char delimiter = ',';
+			state = 2;
+			Sleep(1000);
+			system("cls");
+			Sleep(1000);
+		}
+		if (data.find("MENU_LIST_REQUEST")) {
+			stringstream datalist;
+			string tmp, msgCD, msg;
+			datalist.clear();
+			datalist << data;
+			char delimiter = ',';
+			while (getline(datalist, tmp, delimiter)) {
+				cout << tmp << endl;
+				if (tmp.find("ACCOUNT_ID") != string::npos) {
+					account = tmp.substr(tmp.find(":") + 1);
+				}
+				else if (tmp.find("ACCOUNT_PASSWD") != string::npos) {
+					passwd = tmp.substr(tmp.find(':') + 1);
+				}
+				else if (tmp.find("STORE_NM") != string::npos) {
+					storeNm = tmp.substr(tmp.find(':') + 1);
+				}
+				else if (tmp.find("TABLE_ID") != string::npos) {
+					tableID = tmp.substr(tmp.find(':') + 1);
+					tableID.erase(tableID.end() - 1);
+				}
+			}
+
+			state = 3;
+		}
+	}
+}
+
+int main(int argc, char** argv) {
+	WSADATA		wsaData;
+	SOCKADDR_IN	servAddr;
+	int			strLen;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		showError("WSAStartup() error!");
+	}
+
+	hSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (hSocket == INVALID_SOCKET) {
+		showError("hSocket() error!");
+	}
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = inet_addr(IP);
+	servAddr.sin_port = htons(PORT);
+
+	if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR) {
+		showError("connect() error!");
+	}
+
+	thread proc1(proc_recv);
+
+
+	string sendData;
+	while (!WSAGetLastError()) {
+		if (state == 1) {
+			sendData = loginProcess();
+			send(hSocket, sendData.c_str(), sendData.length(), 0);
+		}
+		else if (state == 2) {
+			sendData = menuListRequest();
+			cout << sendData << endl;
+			send(hSocket, sendData.c_str(), sendData.length(), 0);
+		}
+		else if (state == 3) {
+			menuSelect();
+		}
+	}
+
+	closesocket(hSocket);
+	WSACleanup();
+	return 0;
+}
+
+
+string loginProcess() {
+	stringstream stream;
+	string json;
+
+	cout << "\n해당 서비스를 이용하기 위해서는 로그인을 하셔야 합니다.\n" << endl;
+	cout << "ACCOUNT ID : ";
+	cin >> account_id;
+	
+	cout << "=================================" << endl;
+	cout << "PASSWORD : ";
+	cin >> account_passwd;
+	
+	cout << "=================================" << endl;
+	cout << "매장명 : ";
+	cin >> store_nm;
+	
+	cout << "=================================" << endl;
+	cout << "테이블 ID : ";
+	cin >> table_id;
+		
+	stream << "GET / HTTP/1.1\r\n\r\n";
+	stream << "Accept : */*\r\n";
+	stream << "Content-type : application/json\r\n";
+	stream << "{\"ACCOUNT_ID\":" << account_id << ",\"ACCOUNT_PASSWD\":" << account_passwd	<< ",\"STORE_NM\":" << store_nm << ",\"TABLE_ID\":" << table_id << "}";
+	json = stream.str();
+	state = 0;
+	return json;
+}
+
+string menuListRequest() {
+	stringstream stream;
+	string json;
+
+	stream << "GET / HTTP/1.0\r\n\r\n";
+	stream << "Accept : */*\r\n";
+	stream << "Content-type : application/json\r\n";
+	stream << "{\"STORE_NM\":" << store_nm << "}";
+	json = stream.str();
+	state = 0;
+	return json;
+}
+
+void menuSelect() {
+	cout << "메뉴 리스트를 불러옵니다" << endl;
+}
+
+void showError(const char* msg) {
+	cout << "에러 : " << msg << endl;
+	exit(-1);
+}
---
+++ TableOrderServer/TableOrderServer.cpp
@@ -0,0 +1,162 @@
+﻿#include <WinSock2.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+#include <sql.h>
+#include <sqltypes.h>
+#include <sqlext.h>
+#include <thread>
+#include <sstream>
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+#define PORT 8080
+
+SOCKET          hServSock;
+SOCKET          hClntSock;
+
+SQLHENV henv;
+SQLRETURN ret;
+SQLHDBC hdbc;
+SQLWCHAR* connectionString;
+SQLHSTMT hstmt;
+
+void showError(const char* msg)
+{
+    cout << "에러 : " << msg << endl;
+    exit(-1);
+}
+
+void proc_recvs() {
+    char buffer[1024] = { 0 };
+    string data;
+    while (!WSAGetLastError()) {
+        ZeroMemory(&buffer, 1024);
+        recv(hClntSock, buffer, 1024, 0);
+        cout << "받은 메세지 : " << buffer << endl;
+        data = buffer;
+        data = data.substr(data.find('{'));
+
+        stringstream datalist;
+
+        datalist.clear();
+        datalist << data;
+        char delimiter = ',';
+        if (data.find("ACCOUNT_ID")) {
+            string tmp, account, passwd, storeNm, tableID;
+            while (getline(datalist, tmp, delimiter)) {
+                cout << tmp << endl;
+                if (tmp.find("ACCOUNT_ID") != string::npos) {
+                    account = tmp.substr(tmp.find(":")+1);
+                }
+                else if (tmp.find("ACCOUNT_PASSWD") != string::npos) {
+                    passwd = tmp.substr(tmp.find(':')+1);
+                }
+                else if (tmp.find("STORE_NM") != string::npos) {
+                    storeNm = tmp.substr(tmp.find(':')+1);
+                }
+                else if (tmp.find("TABLE_ID") != string::npos) {
+                    tableID = tmp.substr(tmp.find(':')+1);
+                    tableID.erase(tableID.end()-1);
+                }
+            }
+            cout << account << "/" << passwd << "/" << storeNm << "/" << tableID << endl;
+            stringstream reply;
+            string json;
+            reply << "{\"MSG_CD\":0000" << ",\"MSG\":ACCOUNT_LOGIN SUCCESS}";
+            json = reply.str();
+            send(hClntSock, json.c_str(), json.length(), 0);
+        }
+        else if (data.find("STORE_NM")) {
+            string tmp, storeNm;
+            while (getline(datalist, tmp, delimiter)) {
+                cout << tmp << endl;
+                if (tmp.find("STORE_NM") != string::npos) {
+                    storeNm = tmp.substr(tmp.find(':') + 1);
+                }
+            }
+
+
+        }
+        
+        
+    }
+}
+
+int main(int argc, char** argv)
+{
+    
+    ret = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
+
+    ret = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (void*)SQL_OV_ODBC3, 0);
+
+    
+    ret = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);
+
+    connectionString = (SQLWCHAR*)L"DRIVER={SQLServer};SERVER=serverIP;DATABASE=DataBaseName;UID=username;PWD=passwd";
+
+    ret = SQLDriverConnect(hdbc, NULL, connectionString, SQL_NTS, NULL, 0, NULL, SQL_DRIVER_COMPLETE);
+    //쿼리 실행을 위한 문장 핸들 생성
+    ret = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+    
+
+    WSADATA         wsaData;
+    SOCKADDR_IN     servAddr;
+    SOCKADDR_IN     clntAddr;
+
+    long valread;
+
+    int szClntAddr;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        showError("WSAStartup() error!");
+    }
+
+    hServSock = socket(PF_INET, SOCK_STREAM, 0);
+    if (hServSock == INVALID_SOCKET) {
+        showError("socket() error!");
+    }
+
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons(PORT);
+
+    if (bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR) {
+        showError("bind() error!");
+    }
+    
+    if (listen(hServSock, 5) == SOCKET_ERROR) {
+        showError("listen() error!");
+    }
+
+
+    cout << "클라이언트를 기다립니다...." << endl;
+    cout << "-----------------------------\n" << endl;
+    szClntAddr = sizeof(clntAddr);
+    hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &szClntAddr);
+    if (hClntSock == INVALID_SOCKET) {
+        showError("accept() error!");
+    }
+    if (!WSAGetLastError()) {
+        cout << "연결 완료" << endl;
+        cout << "Client IP : " << inet_ntoa(clntAddr.sin_addr) << endl;
+        cout << "Port : " << ntohs(clntAddr.sin_port) << endl;
+    }
+
+    thread proc(proc_recvs);
+
+    char buffer[10000] = { 0 };
+    while (!WSAGetLastError()) {
+        
+    }
+    
+    proc.join();
+
+    closesocket(hClntSock);
+    closesocket(hServSock);
+    ::WSACleanup();
+    return 0;
+}
